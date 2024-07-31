8951c38fcf02b7071732af8e6d64fbfe1dfc732c
akbarsharofiddinov <akbarjonsharofiddinov2004@gmail.com>
2024-07-31 16:28:33 +0500

    Modal debugging.

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,11 +2,12 @@ const mediaImages = document.querySelectorAll(".img-box");
 const modal = document.querySelector(".modal");
 const modalInner = document.querySelector(".modal-inner");
 
+const imgElement = document.createElement("img");
+imgElement.setAttribute("alt", "image");
+
 mediaImages.forEach((image, index) => {
   image.addEventListener("click", () => {
     modal.classList.add("active");
-    const imgElement = document.createElement("img");
-    imgElement.setAttribute("alt", "image");
     imgElement.setAttribute("src", image.children[0].getAttribute("src"));
     modalInner.append(imgElement);
   });
@@ -15,4 +16,13 @@ mediaImages.forEach((image, index) => {
 const closeModal = document.querySelector(".close-modal");
 closeModal.addEventListener("click", () => {
   modal.classList.remove("active");
+
+  imgElement.removeAttribute("src");
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode === 27) {
+    modal.classList.remove("active");
+    imgElement.removeAttribute("src");
+  }
 });
